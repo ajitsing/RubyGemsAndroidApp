7cee05f4604b3ec83560f254ed65dacb1c29f8b4
AjitSingh <jeetsingh.ajit@gmail.com>
2017-03-01 23:24:26 +0530

    Added search icon in the menu

--- app/src/main/java/com/singhajit/rubygems/dashboard/MainActivity.java
+++ app/src/main/java/com/singhajit/rubygems/dashboard/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.singhajit.rubygems.R;
 import com.singhajit.rubygems.core.INotifier;
@@ -29,16 +30,22 @@ public class MainActivity extends NetworkActivity implements INotifier {
     tabLayout.setupWithViewPager(viewPager);
   }
 
-  private void setupViewPager(ViewPager viewPager) {
-    ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-    adapter.addFragment(new ViewPagerFragment(new TrendingGemsFragment(), getString(R.string.trending)));
-    adapter.addFragment(new ViewPagerFragment(new UserProfileFragment(), getString(R.string.profile)));
-    viewPager.setAdapter(adapter);
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
   }
 
   @Override
   public void notify(String message) {
     Snackbar.make(findViewById(R.id.dashboard), message, Snackbar.LENGTH_LONG);
+  }
+
+  private void setupViewPager(ViewPager viewPager) {
+    ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+    adapter.addFragment(new ViewPagerFragment(new TrendingGemsFragment(), getString(R.string.trending)));
+    adapter.addFragment(new ViewPagerFragment(new UserProfileFragment(), getString(R.string.profile)));
+    viewPager.setAdapter(adapter);
   }
 }
 
