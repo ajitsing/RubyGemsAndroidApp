4416c8a1a8612e5a38220b1692b8f6cf367f8364
AjitSingh <jeetsingh.ajit@gmail.com>
2017-04-29 10:08:19 +0530

    Integrating Sherlock

--- app/src/main/java/com/singhajit/rubygems/core/RubyGemsApplication.java
+++ app/src/main/java/com/singhajit/rubygems/core/RubyGemsApplication.java
@@ -3,12 +3,14 @@ package com.singhajit.rubygems.core;
 import android.app.Application;
 
 import com.singhajit.rubygems.network.RubyGemsVolley;
+import com.singhajit.sherlock.core.Sherlock;
 
 public class RubyGemsApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
     RubyGemsVolley.getInstance(this).getRequestQueue().start();
+    Sherlock.init(this);
   }
 
   @Override
